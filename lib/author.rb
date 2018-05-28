b90dd6c0c5e6282c7127d55775ebe4e9e7200018
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-28 16:21:12 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,11 +1,14 @@
 class Author
 
-  attr_accessor :name, :posts
+  attr_accessor :name, 
 
   def initialize(name)
     @name = name
+    @posts = []
   end
 
-
+  def posts
+    @posts
+  end
 
 end
