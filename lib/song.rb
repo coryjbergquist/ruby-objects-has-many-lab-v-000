3f649229b54a47ec146ad79b2fffdc70526cdd82
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-28 16:46:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,16 @@
 class Song
 attr_accessor :name, :artist
+@@all = []
 
   def initialize(name)
     @name = name
-
+    @@all << self.new(name)
   end
 
   def artist
     @artist
   end
+
+
 
 end
