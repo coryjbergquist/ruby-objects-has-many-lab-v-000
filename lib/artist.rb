7222f577ce84a474a50995e7a02f84acd7cae40b
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-28 15:54:06 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+require "pry"
+
+class Artist
+attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(name)
+    song = Song.new(name)
+
+    @songs << song
+        song.artist = self
+  end
+
+end
